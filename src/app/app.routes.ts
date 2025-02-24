9d94ee5f079a1d8472737125ba3536c73d34f2be
Vu Ngoc Phuong Dinh <55153046+irisdinh@users.noreply.github.com>
2025-02-24 00:49:34 -0600

    feat: page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    // { path: 'products', loadChildren: () => import('./components/products/products.component').then(m => m.ProductsComponent)},
+    { path: 'cart', component: CartComponent },
+    { path: 'confirmation', component: ConfirmationComponent }
+];
